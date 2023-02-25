1810583d713c972a2ab8cbf7a70ed77386b20e9c
anishazahan <anishazahan13@gmail.com>
2023-02-25 10:21:51 +0600

    action type create for fetching data

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Courses from '../../components/courses/Courses'
 import HeroSection from '../../components/Home/HeroSection'
+import HeroSection2 from '../../components/Home/HeroSection2'
 import PolygonCountSection from '../../components/Home/PolygonCountSection'
 
 const Home = () => {
@@ -9,6 +10,7 @@ const Home = () => {
    <HeroSection></HeroSection>
    <Courses></Courses>
    <PolygonCountSection></PolygonCountSection>
+   <HeroSection2></HeroSection2>
    </>
   )
 }
