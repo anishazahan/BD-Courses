3f11b53ba79ec023105981692999807348375aba
anishazahan <anishazahan13@gmail.com>
2023-02-25 09:25:55 +0600

    responsive courses heading section

--- src/components/courses/Courses.js
+++ src/components/courses/Courses.js
@@ -6,20 +6,20 @@ const Courses = () => {
     <>
    
 
-    <section className="max-w-7xl lg:px-16 px-10 my-20">
+    <section className="max-w-7xl mx-auto lg:px-16 px-5 md:px-10 my-20">
 
          {/* ...........courses heading............... */}
 
         <div className="courses-heading">
-            <div className="flex md:flex-row flex-col justify-between items-center">
+            <div className="flex md:flex-row flex-col justify-start md:justify-between md:items-center space-y-5 md:space-y-0">
             <div className="left">
                 <Link to='/' className='p-[10px] px-5 border-2 border-secondary text-secondary font-bold text-[16px] hover:underline'>Top Popular Course</Link>
-                <div className="flex items-center space-x-2 mt-7">
+                <div className="flex flex-col justify-start md:flex-row md:items-center space-x-2 mt-7 mb-5 md:mb-0">
                     <h2 className='font-bold text-3xl text-slate-900'>Most Popular</h2>
                     <h2 className='font-bold text-3xl text-secondary'>Course</h2>
                 </div>
             </div>
-            <div className="right">
+            <div className="right ">
                 <Link to='/' className='px-4 py-[10px] text-white font-semibold bg-secondary hover:bg-primary duration-300 hover:underline'>View all Courses</Link>
             </div>
             </div>
